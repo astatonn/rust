f564a4b7db7eec7d68989c98c5a1f7928628ab4e
astatonn <lucas.lima.rk@gmail.com>
2024-04-19 14:51:30 -0300

    cargo project

---
+++ hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42.d
@@ -0,0 +1,5 @@
+/home/joomla/Documents/rust/hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42: src/main.rs
+
+/home/joomla/Documents/rust/hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/joomla/Documents/rust/hello_cargo/target/debug/hello_cargo: /home/joomla/Documents/rust/hello_cargo/src/main.rs
